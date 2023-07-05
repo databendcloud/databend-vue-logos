8179adddbaf70e7c1e427f7e0cf8cb3f4eb9eee7
q <787025321@qq.com>
2023-07-05 14:36:38 +0800

    chore: version

--- examples/App.vue
+++ examples/App.vue
@@ -3,7 +3,6 @@
     <div>test</div>
     <div>{{ count }}</div>
     <light-databend-cloudHorizontal-svg width="200"></light-databend-cloudHorizontal-svg>
-    <nd-button @click="handleClick">按钮</nd-button>
   </div>
 </template>
 
@@ -12,9 +11,6 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    
-  },
   data() {
     return {
       count: 0
--- packages/index.ts
+++ packages/index.ts
@@ -1,13 +1,11 @@
 import { App } from 'vue'
-import NdButton from './button'
 import LightDatabendCloudHorizontalSvg from './light-databend-cloudHorizontal-svg'
-const components = [ NdButton, LightDatabendCloudHorizontalSvg ]
+const components = [ LightDatabendCloudHorizontalSvg ]
 const install = (app: App): void => {
   components.map((component) => app.component(component.name, component))
 }
 
 export {
-  NdButton,
   LightDatabendCloudHorizontalSvg
 }
 
